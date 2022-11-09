ee250eabff30c9a543940f1e2ecb41e080488230
Martin <mdm271@georgetown.edu>
2022-11-09 17:55:17 -0300

    flip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
-import './App.css';
-
+import "./App.css";
 
 function App() {
   return (
@@ -9,21 +7,63 @@ function App() {
         <h1 className="titulo"> BookHub </h1>
       </header>
       <div className="library">
-      <div className="bookShelf">
-        <div className="book">Decadencia y Caida del Imperio Romano I (E. Gibbon)</div>
-        <div className="book">Decadencia y Caida del Imperio Romano II (E. Gibbon)</div>
-      </div>
-      <div className="bookShelf">
-      <div className="book">Libro de Arena (J.L. Borges)</div>
-      </div>
-      <div className="bookShelf">
-      <div className="book">Libro de Arena (J.L. Borges)</div>
-      </div>
-      <div className="bookShelf">
-        <div className="book">Decadencia y Caida del Imperio Romano I (E. Gibbon)</div>
-        <div className="book">Decadencia y Caida del Imperio Romano II (E. Gibbon)</div>
-        <div className="book">Decadencia y Caida del Imperio Romano III (E. Gibbon)</div>
-      </div>
+        <div className="bookShelf">
+          <div className="bookShelf-inner">
+            <div className="bookShelf-front">
+              <h2>HISTORIA ANTIGUA I</h2>
+            </div>
+            <div className="bookShelf-back">
+              <div className="book">
+                Decadencia y Caida del Imperio Romano I (E. Gibbon)
+              </div>
+              <div className="book">
+                Decadencia y Caida del Imperio Romano II (E. Gibbon)
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bookShelf">
+          <div className="book">Libro de Arena (J.L. Borges)</div>
+        </div>
+        <div className="bookShelf">
+          <div className="book">Libro de Arena (J.L. Borges)</div>
+        </div>
+        <div className="bookShelf">
+          <div className="book">
+            Decadencia y Caida del Imperio Romano I (E. Gibbon)
+          </div>
+          <div className="book">
+            Decadencia y Caida del Imperio Romano II (E. Gibbon)
+          </div>
+          <div className="book">
+            Decadencia y Caida del Imperio Romano III (E. Gibbon)
+          </div>
+        </div>
+        <div className="bookShelf">
+          <div className="book">
+            Decadencia y Caida del Imperio Romano IV (E. Gibbon)
+          </div>
+        </div>
+        <div className="bookShelf">
+          <div className="book">
+            Decadencia y Caida del Imperio Romano I (E. Gibbon)
+          </div>
+          <div className="book">
+            Decadencia y Caida del Imperio Romano II (E. Gibbon)
+          </div>
+          <div className="book">
+            Decadencia y Caida del Imperio Romano III (E. Gibbon)
+          </div>
+          <div className="book">
+            Decadencia y Caida del Imperio Romano III (E. Gibbon)
+          </div>
+          <div className="book">
+            Decadencia y Caida del Imperio Romano IV (E. Gibbon)
+          </div>
+          <div className="book">
+            Decadencia y Caida del Imperio Romano IV (E. Gibbon)
+          </div>
+        </div>
       </div>
     </div>
   );
