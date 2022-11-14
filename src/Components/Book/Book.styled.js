987e1696a73d0b1856560173f9ca35a8ab116332
jaamblico <951753marty.0>
2022-11-14 17:50:17 -0300

    Book Component

--- src/Components/Book/Book.styled.js
+++ src/Components/Book/Book.styled.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components'
 
-export const Quote = styled.h4`
-  text-align: center;
-  font-style: italic;
-  border: 1px solid black;
-  border-radius: 16px;
-  padding: 0.5em;
+export const Book = styled.div`
+margin-top: 0.5em;
+margin-bottom: 0.5em;
+padding: 0.5em;
+border-style: double;
+border-color: red;
+align-content: center;
+justify-content: center;
+transform: rotate(-90deg);
+height: 100%;
+font-size: 70%;
+box-sizing: content-box;
+overflow: hidden;
+text-overflow: ellipsis;
+word-wrap: break-word;
+display: flex;
+line-height: 1em;
+max-height: 5em;
+min-height: 3em;
+min-width: 6em;
+max-width: 10em;
 `
