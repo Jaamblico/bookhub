f86bb033e8c3774311dc5cf457e4efd7396f6a13
jaamblico <951753marty.0>
2022-11-15 16:52:52 -0300

    Library Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import "./App.css";
 import {Book} from "./Components/Book"
+import {Library} from "./Components/Library"
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <h1 className="titulo"> BookHub </h1>
+        <h1 className="titulo"> [ BookHub ] </h1>
       </header>
-      <div className="library">
+        <Library>
         <div className="bookShelf">
           <div className="bookShelf-inner">
             <div className="bookShelf-front">
               <h2>HISTORIA ANTIGUA I</h2>
             </div>
+
             <div className="bookShelf-back">
               <Book Content='Decadencia y Caida del Imperio Romano I (E. Gibbon)'/>
+              <Book Content='Decadencia y Caida del Imperio Romano II (E. Gibbon)'/>
+              <Book Content='Decadencia y Caida del Imperio Romano III (E. Gibbon)'/>
             </div>
+
           </div>
+
         </div>
-      </div>
+
+      </Library>
     </div>
+    </>
   );
 }
 
